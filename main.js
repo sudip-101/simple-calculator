f88ad45319ec4b88cba8ac1d9537a96abef006c8
sudip101-arc <maitysudip6524@gmail.com>
2021-08-07 10:35:39 +0530

    fixed js issues

--- main.js
+++ main.js
@@ -7,28 +7,47 @@ const two = document.getElementById('two');
 const three = document.getElementById('three');
 
 one.onclick = function () {
-    circleBtn.style.marginLeft = '0px';
+    circleBtn.style.left = "0";
+    // circleBtn.style.marginLeft = '0';
+    circleBtn.style.transform = 'translateX(0)'
     body.classList.add('blue-theme');
     body.classList.remove('purple-theme', 'light-theme');
 }
 
 two.onclick = function () {
-    circleBtn.style.marginLeft = '20px';
+    circleBtn.style.left = "50%";
+    // circleBtn.style.left = "calc(50% - 12px)";
+    // circleBtn.style.marginLeft = '20px';
+    circleBtn.style.transform = 'translateX(-50%)'
     body.classList.add('light-theme');
     body.classList.remove('purple-theme', 'blue-theme');
 }
 
 three.onclick = function () {
-    circleBtn.style.marginLeft = '36px';
+    circleBtn.style.left = "100%";
+    // circleBtn.style.left = "calc(100% - 24px)";
+    // circleBtn.style.marginLeft = '36px';
+    circleBtn.style.transform = 'translateX(-100%)'
     body.classList.add('purple-theme');
     body.classList.remove('blue-theme', 'light-theme');
 }
 
+// backSpace = function ()
+// {
+//     var str= $("#value_input").val();
+//     var position = document.getElementById('value_input').selectionStart-1;
+
+//     str = str.substr(0, position) + '' + str.substr(position + 1);
+//     $("#value_input").val(str);
+// }
+
 backSpace = function ()
 {
-    var str= $("#value_input").val();
-    var position = document.getElementById('value_input').selectionStart-1;
+    const input = document.getElementById('value_input');
+    let str= input.value;
+    const position = input.selectionStart-1;
 
     str = str.substr(0, position) + '' + str.substr(position + 1);
-    $("#value_input").val(str);
+    input.value = str;
 }
+
